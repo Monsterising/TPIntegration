92278c4c2dfbd371de728577a19e6d2bc376b32f
Monsterising <wordoflrx@gmail.com>
2015-01-12 20:50:00 +0800

    flash to servers

--- TPIntegration/src/main/java/com/tpintegration/controller/SerialBeanController.java
+++ TPIntegration/src/main/java/com/tpintegration/controller/SerialBeanController.java
@@ -1,7 +1,10 @@
 package com.tpintegration.controller;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,5 +14,17 @@ public class SerialBeanController {
 	public ModelAndView index(){
 		 ModelAndView mv = new ModelAndView("/serialPort/index");
 		return mv;
+	}
+	
+	@RequestMapping(value="transmit")
+	public @ResponseBody HashMap<String,String> transmit(){
+		HashMap<String,String> result = new HashMap<String,String>();
+		result.put("result", "hello client");
+		return result;
+	}
+	@RequestMapping(value="transmit2")
+	public @ResponseBody String transmit2(){
+		String result = "hello client";
+		return result;
 	}
 }
